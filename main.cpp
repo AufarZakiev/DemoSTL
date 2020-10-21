472c24ccb844b320505ae3002482220c01b05a3c
Aufar Zakiev <zaufar@gmail.com>
2020-10-22 00:55:42 +0300

    Proper Nan used

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <iostream>
 #include <vector>
+#include <limits>
 
 using namespace std;
 
@@ -45,12 +46,7 @@ std::vector<Point> extract(const std::vector<Point>& points)
   {
     if (points[i].x >= 0)
     {
-      result.clear();
-      Point nan;
-      nan.x = std::sqrt(-1);
-      nan.x = std::sqrt(-1);
-      result.push_back(nan);
-      return result;
+      return { Point{ std::numeric_limits<int>::quiet_NaN(), std::numeric_limits<int>::quiet_NaN() } };
     }
     result.push_back(points[i]);
     if (++i >= points.size())
